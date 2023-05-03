e46048c19133b301191da4dccc12c8af2a44001f
Muhammed Shaheel <90700629+smileyhacker14@users.noreply.github.com>
2023-05-03 13:26:17 +0530

    Update Session.class.php

--- libs/include/Session.class.php
+++ libs/include/Session.class.php
@@ -5,13 +5,18 @@ class Session
     private $data;
     private $conn;
     public $uid;
+    public $token;
 
     public static function authenticate($user, $pass)
     {
         //Rename login function
         $username = User::login($user, $pass);
         if ($username) {
-            $user = new User($username);
+            try{
+                $user = new User($username);
+            } catch(Exception){
+                return false;
+            }
             $conn = Database::getConnection();
             $ip = $_SERVER['REMOTE_ADDR'];
             $agent = $_SERVER['HTTP_USER_AGENT'];
@@ -21,7 +26,7 @@ class Session
             if ($conn->query($sql)) {
                 $_SESSION['user_id'] = $user->id;
                 $_SESSION['user_name'] = $user->username_user;
-                setcookie("sessionToken", $token, strtotime('+30 days'));
+                setcookie("sessionToken", $token, strtotime('+3 days'));
                 return $token;
             } else {
                 return false;
@@ -35,13 +40,10 @@ class Session
     {
         try {
             $Session = new Session($token);
-            if (isset($_SERVER['REMOTE_ADDR']) and isset($_SERVER['HTTP_USER_AGENT'])) {
-                if($_SERVER['REMOTE_ADDR'] == $Session->getIP()) {
-                    if ($_SERVER['HTTP_USER_AGENT'] == $Session->getUserAgent()) {
-                        return $Session->getUser();
-                    }
-                }
+            if($Session){
+                return $Session->token;
             }
+
         } catch(Exception) {
             return false;
         }
@@ -51,32 +53,16 @@ class Session
     {
         $this->conn = Database::getConnection();
         $this->data = null;
-        $sql = "SELECT * FROM `session` WHERE `token`='$token' LIMIT 1";
+        $sql = "SELECT * FROM `userSession` WHERE `token`='$token' LIMIT 1";
         $result = $this->conn->query($sql);
         if ($result->num_rows) {
             $row = $result->fetch_assoc();
             $this->data = $row;
+            $this->token = $row['token'];
         } else {
-            throw new Exception("Session is invalid.");
+            return false;
         }
     }
-
-    public function getUser()
-    {
-        return new User($this->uid);
-    }
-
-
-    public function getIP()
-    {
-        return isset($this->data["ip"]) ? $this->data["ip"] : false;
-    }
-
-    public function getUserAgent()
-    {
-        return isset($this->data["user_agent"]) ? $this->data["user_agent"] : false;
-    }
-
 
 
     public static function removeUserSesstion($token)
